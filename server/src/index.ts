92a43c27f966d793e54c33f103e4787f1d66b7b7
Daxe9 <davidexie694@gmail.com>
2022-04-06 12:36:18 +0200

    fixed: cannot connect to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,44 @@
+import { createServer } from "http";
 import WebSocket from "ws";
-// server
-const wss = new WebSocket.Server({
-    port: 8081,
-});
+const server = createServer();
+const wss = new WebSocket.Server({ server });
 
 function time() {
     return new Date().toLocaleString();
 }
 
-wss.on("connection", (ws: WebSocket) => {
-    console.log("connected to server");
+let clients: WebSocket[] = [];
 
+function broadcast(msg: string) {
+    clients.forEach((client) => {
+        client.send(msg);
+    });
+}
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+wss.on("connection", async (ws: WebSocket) => {
+    let lastMessageTimestamp: number;
+    clients.push(ws);
+    console.log("connected to server");
     ws.on("message", (msg: string) => {
-        console.log(`Client has sent: ${msg}`);
-        ws.send(
+        if (lastMessageTimestamp && Date.now() - lastMessageTimestamp < 5000) {
+            return;
+        }
+        lastMessageTimestamp = Date.now();
+        console.log("ms" + msg);
+        broadcast(
             JSON.stringify({
                 timestamp: time(),
                 message: new String(msg),
             })
         );
     });
-
+    await sleep(5000);
     wss.on("close", () => {
+        clients.slice(clients.indexOf(ws), 1);
         console.log("disconnected from server");
     });
 });
+
+server.listen(8080);
--- vFrontEnd/src/main.ts
+++ vFrontEnd/src/main.ts
@@ -1,6 +1,8 @@
-const url = `ws://${window.location.hostname}:8081`;
+// replace the url with static ip address
+// advised by rizzotti
+
+const url = `ws://${window.location.hostname}:8080`;
 const ws = new WebSocket(url);
-console.log(url);
 interface Message {
     timestamp: string;
     message: string;
@@ -10,20 +12,23 @@ ws.addEventListener("open", () => {
     console.log("Connected to server");
     document.getElementById("btn").addEventListener("click", () => {
         ws.send(document.querySelector("textarea").value);
+        document.querySelector("textarea").value = "";
     });
-    document.querySelector("textarea").value = "";
 });
 
 // HTTP Authentication failed; no valid credentials available when use web sockets
 
 ws.addEventListener("message", (e) => {
     const msg: Message = JSON.parse(e.data);
-    console.log(msg);
+
+    // create text block from an object
     const newTextBlock: HTMLElement = document.createElement("div");
     const time: HTMLElement = document.createElement("p");
     time.innerText = msg.timestamp;
     const message: HTMLElement = document.createElement("p");
     message.innerText = msg.message;
+
+    // append new text block in the page
     newTextBlock.appendChild(time);
     newTextBlock.appendChild(message);
     document.querySelector("#chat").appendChild(newTextBlock);
