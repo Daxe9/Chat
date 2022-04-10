08769d5e351299f163c6603ad5f4677ac45758b4
Daxe9 <davidexie694@gmail.com>
2022-04-10 21:54:51 +0200

    added endpoint to clear messages and reformatted code

--- server/src/modules/socket.ts
+++ server/src/modules/socket.ts
@@ -1,38 +1,62 @@
-import {Server, Socket} from "socket.io";
-import {Message} from "../types";
+import { Server, Socket } from "socket.io";
+import { Message } from "../types";
 import Database from "./db";
-import "dotenv/config"
+import "dotenv/config";
 
-const db = new Database({
-    user: process.env.DB_USER as string,
-    port: parseInt(process.env.DB_PORT!) as number,
-    database: process.env.DATABASE as string,
-    password: process.env.DB_PASSWORD as string
-}, process.env.DB_TABLE as string)
+const db = new Database(
+    {
+        user: process.env.DB_USER as string,
+        port: parseInt(process.env.DB_PORT!) as number,
+        database: process.env.DATABASE as string,
+        password: process.env.DB_PASSWORD as string
+    },
+    process.env.DB_TABLE as string
+);
+
+// handle message history
+async function getHistory(socket: Socket) {
+    // get all message history when connected
+    const messageHistory: Message[] = await db.getAllMessage();
+    if (messageHistory) {
+        socket.emit("messageHistory", messageHistory);
+    }
+}
+
+// handle disconnection
+function disconnection(socket: Socket) {
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+}
+
+function handleMessage(io: Server, socket: Socket) {
+    // waiting for message and then broadcast it
+    socket.on("msg", async (data: Message) => {
+        // store message into db
+        await db.storeMessage({
+            author: data.author,
+            content: data.content,
+            timestamp: data.timestamp,
+            to_author: data.to_author
+        });
+        // broadcast
+        io.emit("broadcastMessage", data);
+    });
+}
+
+function clearAllMessage(socket: Socket) {
+    socket.on("clearAllMessage", async (name: string) => {
+        await db.clearAllMessageByName(name);
+    });
+}
 
 export function webSocket(io: Server) {
     io.on("connection", async (socket: Socket) => {
         console.log("A user connected");
-        // get all message history when connected
-        const messageHistory: Message[] = await db.getAllMessage();
-        if(messageHistory){
-            socket.emit("messageHistory", messageHistory)
-        }
 
-        // waiting for message and then broadcast it
-        socket.on("msg", async (data: Message) => {
-            // store message into db
-            await db.storeMessage({
-                author: data.author,
-                content: data.content,
-                timestamp: data.timestamp,
-                to_author: data.to_author
-            })
-            // broadcast
-            io.emit('broadcastMessage', data)
-        })
-        socket.on("disconnect", () => {
-            console.log("A user disconnected");
-        })
+        await getHistory(socket);
+        handleMessage(io, socket);
+        clearAllMessage(socket);
+        disconnection(socket);
     });
 }
